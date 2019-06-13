c8b1c26e2bd0cb0fc237bc59cf28a18e9496c31d
Viet Nguyen <vietjul@gmail.com>
2019-06-12 22:29:52 -0400

    feat: add favicon, remove old style

--- teach-now/src/App.js
+++ teach-now/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 import {CandidateByCountryChart} from './charts';
@@ -7,20 +7,6 @@ import {CandidateByCountryChart} from './charts';
 function App() {
   return (
     <div className="chart">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <CandidateByCountryChart />
     </div>
   );
